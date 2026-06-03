b8c88ba9e8badd3100dc9e3246648e152a737618
lamsssaaa <ahmed.salam@hotmail.ch>
2026-06-03 21:02:06 +0200

    Add 'Notre promesse' manifesto section (FR/DE/IT)

---
+++ _next/static/HiPMK7fCvYrZD6rm7Ev3t/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[lang]","\u002F[lang]\u002F[collection]","\u002F[lang]\u002F[collection]\u002F[product]","\u002F[lang]\u002Fcheckout","\u002F[lang]\u002Fcheckout\u002Fconfirmation","\u002F[lang]\u002Finfos\u002F[slug]","\u002Fsitemap\u002F[__metadata_id__]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
